339e70ec2042257da6c02f90ba496f52e487ce9c
madiharuhe <154939368+madiharuhe@users.noreply.github.com>
2024-08-17 08:17:12 +0530

    Update p11.py

--- p11.py
+++ p11.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 X, Y = load_iris(return_X_Y=True)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
-y_pred = KNeighborsClassifier(5).fit(X_train, Y_train).predict(X_test)
+Y_pred = KNeighborsClassifier(5).fit(X_train, Y_train).predict(X_test)
 
 print(confusion_matrix(Y_test, Y_pred))
 print(classification_report(Y_test, Y_pred))
